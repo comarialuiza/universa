6457d82b029e359245e486a2aac9d0622e28b9e2
Maria Luiza <co.maria.luiza@gmail.com>
2020-08-18 17:31:30 -0300

    Initiates dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Progress } from 'antd';
 
-import { Container } from './styles';
+import { Container, ProgressTitle, SubContainer } from './styles';
 import Header from '../../components/Header';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Header />
-      <Container></Container>
+      <Container>
+        <SubContainer>
+          <ProgressTitle>Progresso acadêmico</ProgressTitle>
+          <Progress
+            strokeColor={{
+              '0%': '#108ee9',
+              '100%': '#87d068',
+            }}
+            percent={ 25 }
+          />
+        </SubContainer>
+      </Container>
     </>
   );
 }
--- src/pages/Dashboard/styles.tsx
+++ src/pages/Dashboard/styles.tsx
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const ProgressTitle = styled.h3``;
+
+export const SubContainer = styled.div``;
