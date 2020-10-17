0483f45479647984a57498845e2cd2d069897253
Maria Luiza <co.maria.luiza@gmail.com>
2020-10-17 20:52:29 -0300

    Creates selectedClasses section on enrollment

--- src/pages/Enrollment/index.tsx
+++ src/pages/Enrollment/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Header from '../../components/Header';
 import { DownloadOutlined } from '@ant-design/icons';
 
-import { Container, AvailableContainer, SubjectsAvailable, AvailableTitle, SubjectAvailable, SubjectTitle, SubjectTeacher, SubjectHours, SubjectCode } from './styles';
+import { Container, AvailableContainer, SubjectsContainer, Title, Description, SubjectAvailable, SubjectTitle, SubjectTeacher, SubjectHours, SubjectCode, SubjectsList } from './styles';
 import { UnavailableContainer } from '../../styles/global';
 
 import enrollmentMock from './../../utils/enrollmentMockData';
@@ -15,6 +15,7 @@ interface EnrollmentData {
 
 const Enrollment: React.FC = () => {
     const [ enrollmentAvailable ] = useState(true);
+    const [ chosenSubjects, setChosenSubjects ] = useState<EnrollmentData[]>([]);
 
     return (
         <>
@@ -22,17 +23,44 @@ const Enrollment: React.FC = () => {
             <Container className="container">
                 { enrollmentAvailable ? (
                     <AvailableContainer>
-                        <AvailableTitle>Disciplinas disponíveis no semestre 2020.2</AvailableTitle>
-                        <SubjectsAvailable>
-                            { enrollmentMock.map((subject: EnrollmentData) => (
-                                <SubjectAvailable>
-                                    <SubjectTitle>{ subject.title }</SubjectTitle>
-                                    <SubjectCode>{ subject.code }</SubjectCode>
-                                    <SubjectTeacher>{ subject.teacher }</SubjectTeacher>
-                                    <SubjectHours>{ subject.hours } horas</SubjectHours>
-                                </SubjectAvailable>
-                            )) }
-                        </SubjectsAvailable>    
+                        <SubjectsContainer>
+                            <Title>Disciplinas disponíveis no semestre 2020.2</Title>
+                            <Description>Para solicitar uma disciplina, por favor clique na mesma. Ela irá aparecer na lista ao lado.</Description>
+                            <SubjectsList>
+                                { enrollmentMock.map((subject: EnrollmentData) => (
+                                    <SubjectAvailable 
+                                        onClick={ () => {
+                                            setChosenSubjects([...chosenSubjects, subject]);
+                                        } }
+                                    >
+                                        <SubjectTitle>{ subject.title }</SubjectTitle>
+                                        <SubjectCode>{ subject.code }</SubjectCode>
+                                        <SubjectTeacher>{ subject.teacher }</SubjectTeacher>
+                                        <SubjectHours>{ subject.hours } horas</SubjectHours>
+                                    </SubjectAvailable>
+                                )) }
+                            </SubjectsList>
+                        </SubjectsContainer>  
+
+                        <SubjectsContainer>
+                            <Title>Disciplinas a requisitar para o semestre 2020.2</Title>
+                            <Description>Para cancelar a solicitação de uma disciplina, por favor clique na mesma. Ela irá desaparecer da lista abaixo e a solicitação será cancelada.</Description>
+                            <SubjectsList>
+                                { chosenSubjects.length < 1 && <p>Nenhuma disciplina escolhida :( </p>}
+                                { chosenSubjects?.map((subject: EnrollmentData) => (
+                                    <SubjectAvailable 
+                                        onClick={ () => {
+                                            setChosenSubjects([...chosenSubjects, subject]);
+                                        } }
+                                    >
+                                        <SubjectTitle>{ subject.title }</SubjectTitle>
+                                        <SubjectCode>{ subject.code }</SubjectCode>
+                                        <SubjectTeacher>{ subject.teacher }</SubjectTeacher>
+                                        <SubjectHours>{ subject.hours } horas</SubjectHours>
+                                    </SubjectAvailable>
+                                )) }
+                            </SubjectsList>
+                        </SubjectsContainer>  
                     </AvailableContainer>
                 ) : (
                     <UnavailableContainer>
--- src/pages/Enrollment/styles.tsx
+++ src/pages/Enrollment/styles.tsx
@@ -2,29 +2,44 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const AvailableContainer = styled.div``;
+export const AvailableContainer = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+`;
 
-export const SubjectsAvailable = styled.ul`
+export const SubjectsContainer = styled.div`
+    width: 48%;
+`;
+
+export const SubjectsList = styled.ul`
     list-style: none;
-
     max-height: 400px;
-    max-width: 300px;
+    
     overflow: scroll;
 `;
 
-export const AvailableTitle = styled.h3`
+export const Title = styled.h3`
     color: var(--color-primary);
-    font-size: 24px;
+    font-size: 22px;
     font-weight: normal;
     margin-bottom: 20px;
+`;
+
+export const Description = styled.p`
+    color: var(--color-text-complement);
+    font-size: 15px;
+    line-height: 22px;
+    text-align: justify;
 `;
 
 export const SubjectAvailable = styled.li`
     background-color: var(--color-white);
     border-radius: 8px;
     padding: 12px;
-    max-width: 280px;
+    width: calc(100% - 18px);
     position: relative;
+    cursor: pointer;
 
     &:not(:last-of-type) {
         margin-bottom: 20px;
